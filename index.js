0954711b98ab888c147d2d2a5fab8c954217a1a9
ilia-io <likeapu@gmail.com>
2024-10-18 14:27:28 +0300

    add error handling

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const { Bot } = require('grammy')
-require('dotenv').config()
+const { Bot, GrammyError, HttpError, Keyboard } = require('grammy');
+require('dotenv').config();
 
-const bot = new Bot(process.env.BOT_API_KEY)
+const bot = new Bot(process.env.BOT_API_KEY);
+console.log(`Bot is running!`);
 
-bot.start()
+bot.command('start', async (ctx) => {
+  await ctx.reply('Hello World!');
+  console.log(ctx);
+});
 
+bot.hears('html', async (ctx) => {
+  await ctx.reply('Hey there!');
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
+
+bot.start();
